11143d9617a6ee9af638f1e6a58c1fdc38e37be3
leticiaaraujoa <laalvim@sga.pucminas.br>
2025-03-25 23:41:23 +0000

    raio

---
+++ java.js
@@ -0,0 +1,8 @@
+alert ("Informe o raio");
+let raio = parseInt(prompt("Informe o raio:"));
+C=2*3.14159265358979323846*raio;
+A=3.14159265358979323846*raio*raio;
+V=3/4*3.14159265358979323846*raio*raio*raio;
+alert ("O comprimento é" + C);
+alert ("A área é" + A);
+alert ("O volume é" + V);
